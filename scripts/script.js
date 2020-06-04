8360791fc0e69d735411e7f849b2d7c498860132
Liger <jtadub@gmail.com>
2020-06-04 16:12:12 +1200

    Build skeleton of site

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+let myLibrary =[];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    if (read === false) {
+      return `${title} by ${author}, ${pages} pages, not read yet.`;
+    }else {
+      return `${title} by ${author}, ${pages} pages, has been read.`;
+    }
+  }
+}
+
+function addBookToLibrary(input) {
+  
+}
