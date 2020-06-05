740b4abb58435553843170ede802f35e383373be
Liger <jtadub@gmail.com>
2020-06-05 16:09:34 +1200

    Create render() and work on book display format

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-let myLibrary =[];
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -14,6 +12,45 @@ function Book(title, author, pages, read) {
   }
 }
 
-function addBookToLibrary(input) {
-  
+function addBookToLibrary(title, author, pages, read) {
+  book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
+
+function renderBooks() {
+  const bookContainer = document.querySelector(".book-display-container");
+
+  myLibrary.forEach(book => {
+    let bookCard = document.createElement('div');
+    bookCard.classList.add("book-card")
+    let infoList = document.createElement('ul');
+    infoList.classList.add("book-info-list");
+    let title = document.createElement('li');
+    title.append(document.createTextNode(book.title));
+    title.classList.add("book-info");
+    let author = document.createElement('li');
+    author.append(document.createTextNode(book.author));
+    author.classList.add("book-info")
+    let pages = document.createElement('li');
+    pages.append(document.createTextNode(book.pages));
+    pages.classList.add("book-info");
+    let read = document.createElement('li');
+    read.append(document.createTextNode(book.read));
+    read.classList.add("book-info");
+
+    bookContainer.appendChild(bookCard);
+    bookCard.appendChild(infoList);
+    infoList.appendChild(title);
+    infoList.appendChild(author);
+    infoList.appendChild(pages);
+    infoList.appendChild(read);
+  })
+
+}
+
+let myLibrary = [];
+addBookToLibrary('Harry Potter Series', 'J. K. Rowling', '4224', 'Finished');
+addBookToLibrary('Fire Punch', 'Tatsuki Fujimoto', '1664', 'Not yet read');
+addBookToLibrary('Lord of the Rings', 'J. R. R. Tolkien', '1137', 'Reading');
+addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', '281', 'Not Yet read');
+renderBooks();
