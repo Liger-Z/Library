431fc2d6b64406fa74520c393334ec41f7ccf0d9
Liger <jtadub@gmail.com>
2020-06-10 12:25:43 +1200

    Fix renderBook to only add unique books to screen

--- scripts/script.js
+++ scripts/script.js
@@ -15,47 +15,62 @@ function Book(title, author, pages, status) {
 function addBookToLibrary() {
   let bookDetails = document.getElementsByClassName("form-input");
   let statusList = document.getElementsByName("status");
-  console.log(statusList);
+  let status;
   for (let i = 0; i < statusList.length; i++) {
-    console.log(statusList[i].checked);
     if (statusList[i].checked) {
-      var status = statusList[i].value;
+      status = statusList[i].value;
       break;
     }
   }
-  console.log(status);
+  
   book = new Book(bookDetails[0].value, bookDetails[1].value, bookDetails[2].value, status);
-  myLibrary.push(book);
+  console.log(bookInLibrary(book))
+  if (bookInLibrary(book)) {
+    return undefined;
+  }else {
+    myLibrary.push(book);
+  }
+}
+function bookInLibrary(book) {
+  let inLibrary;
+  myLibrary.forEach(bookObj => {
+    if (book.title == bookObj.title && book.author == bookObj.author && book.pages == bookObj.pages && book.status == bookObj.status) {
+      inLibrary = true
+    }
+  });
+
+  return inLibrary ? true : false;
 }
 
-function renderBooks() {
+function renderBook() {
   const bookContainer = document.querySelector(".book-display-container");
+  let book = myLibrary[myLibrary.length - 1];
+  if (currentlyRendered.includes(book)) {return undefined};
 
-  myLibrary.forEach(book => {
-    let bookCard = document.createElement('div');
-    bookCard.classList.add("book-card")
-    let infoList = document.createElement('ul');
-    infoList.classList.add("book-info-list");
-    let title = document.createElement('li');
-    title.append(document.createTextNode(book.title));
-    title.classList.add("book-info");
-    let author = document.createElement('li');
-    author.append(document.createTextNode(book.author));
-    author.classList.add("book-info")
-    let pages = document.createElement('li');
-    pages.append(document.createTextNode(book.pages));
-    pages.classList.add("book-info");
-    let read = document.createElement('li');
-    read.append(document.createTextNode(book.status));
-    read.classList.add("book-info");
+  let bookCard = document.createElement('div');
+  bookCard.classList.add("book-card");
+  let infoList = document.createElement('ul');
+  infoList.classList.add("book-info-list");
+  let title = document.createElement('li');
+  title.append(document.createTextNode(book.title));
+  title.classList.add("book-info");
+  let author = document.createElement('li');
+  author.append(document.createTextNode(book.author));
+  author.classList.add("book-info")
+  let pages = document.createElement('li');
+  pages.append(document.createTextNode(book.pages));
+  pages.classList.add("book-info");
+  let read = document.createElement('li');
+  read.append(document.createTextNode(book.status));
+  read.classList.add("book-info");
 
-    bookContainer.appendChild(bookCard);
-    bookCard.appendChild(infoList);
-    infoList.appendChild(title);
-    infoList.appendChild(author);
-    infoList.appendChild(pages);
-    infoList.appendChild(read);
-  })
+  bookContainer.appendChild(bookCard);
+  bookCard.appendChild(infoList);
+  infoList.appendChild(title);
+  infoList.appendChild(author);
+  infoList.appendChild(pages);
+  infoList.appendChild(read);
+  currentlyRendered.push(book);
 }
 
 function showForm() {
@@ -67,11 +82,12 @@ function showForm() {
 }
 
 let myLibrary = [];
+let currentlyRendered = [];
 const newBookButton = document.querySelector("#new-button");
 newBookButton.addEventListener("click", showForm);
 const addBookButton = document.querySelector("#add-button");
 addBookButton.addEventListener("click", addBookToLibrary);
-addBookButton.addEventListener("click", renderBooks);
+addBookButton.addEventListener("click", renderBook);
 /*
 Possibly sort books into categories i.e. if they have been read or not (or are being read)
 */
